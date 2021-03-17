23b247ab8e84805b1ed5e856ec4fe1590d048b47
MatteoKastler <matteo.kastler@gmail.com>
2021-03-17 16:16:16 +0100

    basecode

--- src/client/Main.java
+++ src/client/Main.java
@@ -1,10 +1,51 @@
 package client;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.nio.CharBuffer;
 
 public class Main {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Socket s = null;
+		DataOutputStream dout = null;
+		try {
+			s = new Socket("localhost", 6666);
+			dout = new DataOutputStream(s.getOutputStream());
+			dout.writeUTF("Hello Server");
+			dout.flush();
 
+			BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+			while (true) {
+			}
+
+		} catch (Exception e) {
+			System.out.println("hier");
+			e.printStackTrace();
+		} finally {
+			try {
+				s.close();
+				dout.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
-
+	public static String getinput() {
+		StringBuilder sb = new StringBuilder();
+		try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+		    CharBuffer buffer = CharBuffer.allocate(1024);
+		    while (reader.read(buffer) != -1) {
+		        buffer.flip();
+		        sb.append(buffer);
+		        buffer.clear();
+		    }
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println(sb);
+		return sb.toString();
+	}
 }
--- src/server/Main.java
+++ src/server/Main.java
@@ -1,10 +1,34 @@
 package server;
+import java.io.DataInputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		try {
+			PrintWriter out = null;
+			ServerSocket sock = new ServerSocket(6666);
+			System.out.println("Server wartet....");
+			int noRequests = 0;
+			while (noRequests < 1000) {
+				Socket s = sock.accept();
+				DataInputStream dis = new DataInputStream(s.getInputStream());
+				String str = (String) dis.readUTF();
+				System.out.println("message= " + str);
+				System.out.println("request:" + noRequests);
 
+				out = new PrintWriter(new OutputStreamWriter(s.getOutputStream()));
+				out.write("Echo:" + str + "\n");
+				out.flush();
+				noRequests++;
+			}
+			out.close();
+			sock.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
-
 }
