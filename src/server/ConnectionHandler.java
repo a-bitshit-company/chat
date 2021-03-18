4ac19dfbfb6ab5df143aa94657399e0f5fd59123
Felix Perktold <felix.perktold@live.at>
2021-03-18 16:17:18 +0100

    forgot to push new class

---
+++ src/server/ConnectionHandler.java
@@ -0,0 +1,25 @@
+package server;
+
+import java.util.ArrayList;
+import java.io.*;	
+import java.net.*;
+
+public class ConnectionHandler extends Thread {
+	ServerSocket sock;
+	
+	public ConnectionHandler(ServerSocket sock){
+		this.sock=sock;
+	}
+	
+	ArrayList<Socket> connections = new ArrayList<Socket>();
+	public void run() {
+		for(;;) {
+			try {
+				this.connections.add(sock.accept());
+				System.out.println("CON: new connection ("+connections.size()+")");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
